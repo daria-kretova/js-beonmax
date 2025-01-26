de21bed3e35733baee5a8263f17db8294545115a
daria-kretova <kretovadar@yandex.ru>
2025-01-26 19:06:54 +0300

    objects comments

--- objects.js
+++ objects.js
@@ -6,7 +6,7 @@ let options = {
     name: "test"
 };
 
-// получить ссвойство объекта можно через точку или квадратные скобки options.name = выведется test
+// получить ссвойство объекта можно через точку options.name = выведется test или квадратные скобки options[key]
 
 console.log(options.name);
 options.bool = false; // добавление свойств объекту
@@ -20,7 +20,11 @@ delete options.bool; // удаление свойства из объекта
 console.log(options);
 
 
+// key - переменная которая обозначает каждое свойство или каждый методок который у нас есть в объекте - ширина, высота, имя и т.д.
 for (let key in options) {
     console.log('Свойство ' + key + ' имеет значение ' + options[key]);
 }
 console.log(Object.keys(options).length)
+
+
+// contrl+alt+N - запуск в консоли всей команды на странице
